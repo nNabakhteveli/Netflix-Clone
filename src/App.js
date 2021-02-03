3f8498ee9bbdb40436bd024f135c923b70973356
Nabakhteveli <nabakhtevelin@gmail.com>
2021-02-03 17:53:04 +0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="first-P">Unlimited movies, TV</p>
+        <p className="sec-P">shows, and more.</p>
+        <p className="third-P">Watch anywhere. Cancel anytime.</p>
+        <p className="fourth-P">Ready to watch? Enter your email to create or restart your membership.</p>
+        <div className="div1">
+          <input placeholder=" Your Email Address" className="email-input"/>
+          <button className="button-within-input">TRY 30 DAYS FREE</button>
+        </div>     
       </header>
     </div>
   );
