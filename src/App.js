be29a182ddce26baa3bc5e49694f0b8cf79a2e1e
Nabakhteveli <nabakhtevelin@gmail.com>
2021-02-03 19:20:03 +0400

    Basic styling of main page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="https://i.ibb.co/mvqhz8N/netflix-logo-png-2584.png" className="netflix-logo" alt="Netflix Logo"/>
+        <button className='sign-in-button'>Sign in</button>
         <p className="first-P">Unlimited movies, TV</p>
         <p className="sec-P">shows, and more.</p>
         <p className="third-P">Watch anywhere. Cancel anytime.</p>
@@ -12,6 +14,7 @@ function App() {
         <div className="div1">
           <input placeholder=" Your Email Address" className="email-input"/>
           <button className="button-within-input">TRY 30 DAYS FREE</button>
+          <p className="fifth-P">Only new members are eligible for this offer.</p>
         </div>     
       </header>
     </div>
