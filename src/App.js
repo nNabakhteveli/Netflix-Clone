b7e388b9bb24bd75bbe449f9e69cf2a96c10cc98
Nabakhteveli <nabakhtevelin@gmail.com>
2021-02-08 17:01:15 +0400

    Adding fourth page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
 
+function FourthPage(){
+  return(
+    <div className="fourth-page">
+        <p className="fourth-first-p">Watch everywhere.</p>
+        <p className='fourth-sec-p'>Stream unlimited movies and TV shows on</p>
+        <p className="fourth-span-1">your phone, tablet, laptop, and TV without</p>
+        <span className='fourth-span-2'>something to watch.</span>
+        <div>
+          <img className="collage-img" src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png' alt='mobile' />
+        </div>
+        <hr className="fourth-page-hr" />
+    </div>
+  );
+}
+
+
 function ThirdPage(){
   return(
     <div className="third-page">
@@ -46,6 +62,7 @@ function App() {
       </header>
       <SecondPage />
       <ThirdPage />
+      <FourthPage />
     </div>
   );
 }
