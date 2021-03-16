5383e94b0344dc02046d4276c88d0cfc4ebc2e52
Nika <nabakhtevelin@gmail.com>
2021-03-16 18:08:45 +0400

    Adding copyright

--- src/modules/footer/footer.js
+++ src/modules/footer/footer.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 
 class Copyright extends React.Component {
   render(){
-      return (
-          <div class='copyright'>
-              <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-              </header>
-          </div>
-      );
+    return (
+      <div class='copyright'>
+        <div className="copy-text">
+          <span>Made by Nika Nabakhteveli,</span><br />
+          <span>By powers of:</span>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
   }
 }
 
@@ -17,34 +19,34 @@ export class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
-          <a href='https://help.netflix.com/en/contactus' className='questions' target='_blank' style={{textDecoration:'none', color: 'grey'}}><p>Question? Contact us.</p></a>
+          <a href='https://help.netflix.com/en/contactus' className='questions' target='_blank'><p>Question? Contact us.</p></a>
         <div>
         </div>
         <div className="firstFour">
-          <a href='https://help.netflix.com/en/node/412' style={{textDecoration:'none', color: 'grey'}}><p>FAQ</p></a>
-          <a href='https://ir.netflix.net/ir-overview/profile/default.aspx' style={{textDecoration:'none', color: 'grey'}}><p>Investor Relations</p></a>
-          <a href='https://help.netflix.com/legal/privacy' style={{textDecoration:'none', color: 'grey'}}><p>Privacy</p></a>
-          <a href='https://fast.com/' style={{textDecoration:'none', color: 'grey'}}><p>Speed Test</p></a>
+          <a className='list-item' href='https://help.netflix.com/en/node/412'><p>FAQ</p></a>
+          <a className='list-item' href='https://ir.netflix.net/ir-overview/profile/default.aspx'><p>Investor Relations</p></a>
+          <a className='list-item' href='https://help.netflix.com/legal/privacy'><p>Privacy</p></a>
+          <a className='list-item' href='https://fast.com/'><p>Speed Test</p></a>
         </div>
   
         <div className="secondFour">
-          <a href='https://help.netflix.com/en/' style={{textDecoration:'none', color: 'grey'}}><p>Help Center</p></a>
-          <a href='https://jobs.netflix.com/' style={{textDecoration:'none', color: 'grey'}}><p>Jobs</p></a>
-          <a href='https://help.netflix.com/legal/privacy#cookies' style={{textDecoration:'none', color: 'grey'}}><p>Cookie Preferences</p></a>
-          <a href='https://help.netflix.com/legal/notices' style={{textDecoration:'none', color: 'grey'}}><p>Legal Notices</p></a>
+          <a className='list-item' href='https://help.netflix.com/en/'><p>Help Center</p></a>
+          <a className='list-item' href='https://jobs.netflix.com/'><p>Jobs</p></a>
+          <a className='list-item' href='https://help.netflix.com/legal/privacy#cookies'><p>Cookie Preferences</p></a>
+          <a className='list-item' href='https://help.netflix.com/legal/notices'><p>Legal Notices</p></a>
         </div>
   
         <div className="thirdFour">
-          <a href='https://www.netflix.com/ge/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fyouraccount' style={{textDecoration:'none', color: 'grey'}} ><p>Account</p></a>
-          <a href='https://devices.netflix.com/en/' style={{textDecoration:'none', color: 'grey'}}><p>Ways to Watch</p></a>
-          <a href='https://help.netflix.com/legal/corpinfo' style={{textDecoration:'none', color: 'grey'}}><p>Coprorate Information</p></a>
-          <a href='https://www.netflix.com/ge/browse/genre/839338' style={{textDecoration:'none', color: 'grey'}}><p>Netflix Originals</p></a>
+          <a className='list-item' href='https://www.netflix.com/ge/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fyouraccount'><p>Account</p></a>
+          <a className='list-item' href='https://devices.netflix.com/en/'><p>Ways to Watch</p></a>
+          <a className='list-item' href='https://help.netflix.com/legal/corpinfo'><p>Coprorate Information</p></a>
+          <a className='list-item' href='https://www.netflix.com/ge/browse/genre/839338'><p>Netflix Originals</p></a>
         </div>
   
         <div className="fourthFour">
-          <a href='https://media.netflix.com/en/' style={{textDecoration:'none', color: 'grey'}}><p>Media Center</p></a>
-          <a href='https://help.netflix.com/legal/termsofuse' style={{textDecoration:'none', color: 'grey'}}><p>Terms of Use</p></a>
-          <a href='https://help.netflix.com/en/contactus' style={{textDecoration:'none', color: 'grey'}}><p>Contact Us</p></a>
+          <a className='list-item' href='https://media.netflix.com/en/'><p>Media Center</p></a>
+          <a className='list-item' href='https://help.netflix.com/legal/termsofuse'><p>Terms of Use</p></a>
+          <a className='list-item' href='https://help.netflix.com/en/contactus'><p>Contact Us</p></a>
         </div>
           
         <div>
@@ -54,7 +56,6 @@ export class Footer extends React.Component {
             <option value='rus'>русский</option>
           </select>
         </div>
-
         <Copyright />
       </div>
     );
